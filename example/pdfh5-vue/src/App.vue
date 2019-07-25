83ac5eb52b305fb3b581f6a576bf2d4fe49ce9a1
gjTool <861366490@qq.com>
2019-07-25 15:15:54 +0800

    增加vue使用例子

---
+++ example/pdfh5-vue/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+      <div id="demo"></div>
+  </div>
+</template>
+
+<script>
+import Pdfh5 from "pdfh5";
+export default {
+  name: 'App',
+  data() {
+    return {
+      pdfh5: null
+    };
+  },
+ mounted() {
+    this.pdfh5 = new Pdfh5("#demo", {
+      pdfurl: "http://www.gjtool.cn/pdfh5/default.pdf",
+      lazy:true 
+    });
+    this.pdfh5.on("render", function (pageNum, time) {
+      console.log("当前加载页码：" + pageNum + "，耗时：" + time + "毫秒，总页数：" + this.totalNum)
+    })
+    this.pdfh5.on("complete", function (status, msg, time) {
+      console.log("状态：" + status + "，信息：" + msg + "，耗时：" + time + "毫秒，总页数：" + this.totalNum)
+    })
+  }
+}
+</script>
+
+<style>
+ @import "pdfh5/css/pdfh5.css";
+ *{
+   padding: 0;
+   margin: 0;
+ }
+ html,body,#app {
+   width: 100%;
+   height: 100%;
+ }
+</style>
---
+++ example/pdfh5-vue/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="demo"></div>
+</template>
+<script>
+import Pdfh5 from "pdfh5";
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      pdfh5: null
+    };
+  },
+  mounted() {
+    this.pdfh5 = new Pdfh5("#demo", {
+      pdfurl: "./assets/default.pdf"
+    });
+    this.pdfh5.on("complete", function (status, msg, time) {
+      console.log("状态：" + status + "，信息：" + msg + "，耗时：" + time + "毫秒，总页数：" + this.totalNum)
+    })
+  }
+};
+</script>
+<style scoped>
+  @import "pdfh5/css/pdfh5.css";
+</style>
