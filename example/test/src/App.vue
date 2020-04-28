e063af2f91034222039c854a0ec634c6b2b4e122
gjTool <305250069@qq.com>
2020-04-28 15:05:11 +0800

    修复bu gbug

---
+++ example/test/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+	<div id="demo"></div>
+  </div>
+</template>
+<script>
+  import Pdfh5 from "pdfh5";
+  export default {
+    name: 'App',
+	data() {
+	  return {
+	    pdfh5: null
+	  };
+	},
+	mounted() {
+		//实例化
+	  this.pdfh5 = new Pdfh5("#demo", {
+		pdfurl: "../../static/test.pdf"
+	  });
+	  //监听完成事件
+	  this.pdfh5.on("complete", function (status, msg, time) {
+		console.log("状态：" + status + "，信息：" + msg + "，耗时：" + time + "毫秒，总页数：" + this.totalNum)
+	  })
+	}
+  }
+</script>
+
+<style>
+	@import "pdfh5/css/pdfh5.css";
+	*{
+	padding: 0;
+	margin: 0;
+	}
+	html,body,#app {
+	width: 100%;
+	height: 100%;
+	}
+</style>
