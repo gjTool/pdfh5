487425af485eecba2add4ab3587a330b2f4e4799
gjTool <305250069@qq.com>
2020-07-15 13:56:27 +0800

    删除多余文件

--- js/pdfh5.js
+++ js/pdfh5.js
@@ -1,8 +1,8 @@
 ;
 (function(g, fn) {
-	var version = "1.3.15",
+	var version = "1.3.16",
 		pdfjsVersion = "2.3.200";
-	console.log("pdfh5.js v" + version + " & https://www.gjtool.cn")
+	console.log("pdfh5.js v" + version + "  https://www.gjtool.cn")
 	if (typeof require !== 'undefined') {
 		if (g.$ === undefined) {
 			g.$ = require('./jquery-1.11.3.min.js');
