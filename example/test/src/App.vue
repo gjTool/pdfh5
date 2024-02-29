91325e8e2a1376eeebc38adc4e946cd6ec775030
gjTool <861366490@qq.com>
2024-02-29 09:56:58 +0800

    修复bugs

--- example/test/src/App.vue
+++ example/test/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app"><div id="demo"></div></div>
+  <div id="app">
+    <div id="demo"></div>
+  </div>
 </template>
 <script>
 import Pdfh5 from 'pdfh5';
@@ -11,25 +13,28 @@ export default {
     };
   },
   mounted() {
-     new Pdfh5('#demo', {
-         pdfurl: "./static/git.pdf"
-       });
-    
-  
+    this.pdfh5 = new Pdfh5('#demo', {
+      pdfurl: "./static/git.pdf"
+    });
+    this.pdfh5.on("scroll", function (scrollTop, currentNum) {
+      console.log("scrollTop:" + scrollTop, "currentNum:" + currentNum);
+    });
+
   }
 };
 </script>
 
 <style>
 @import 'pdfh5/css/pdfh5.css';
+
 * {
   padding: 0;
   margin: 0;
 }
+
 html,
 body,
 #app {
   width: 100%;
   height: 100%;
-}
-</style>
+}</style>
